e7a8b70554367084b2ae30017d5a097bce23b724
rbonnell <ryanbonnell@protonmail.com>
2020-02-06 13:37:10 -0700

    First Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# SleepTrackerRecyclerView
+Author: Ryan Bonnell<br>
+About: Android CodeLabs project with minor customization.  Track sleep quality
+in a database. This version uses RecyclerView/ListAdapter/ItemCallback/@BindingAdapter<br>
+Target API: 28<br>
+Min API: 19
---
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/BindingUtils.kt
@@ -0,0 +1,32 @@
+package com.example.android.trackmysleepquality.sleeptracker
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.example.android.trackmysleepquality.R.drawable.*
+import com.example.android.trackmysleepquality.convertDurationToFormatted
+import com.example.android.trackmysleepquality.convertNumericQualityToString
+import com.example.android.trackmysleepquality.database.SleepNight
+
+@BindingAdapter("sleepDurationFormatted")
+fun TextView.setSleepDurationFormatted(item: SleepNight) {
+    text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, context.resources)
+}
+
+@BindingAdapter("sleepQualityString")
+fun TextView.setSleepQualityString(item: SleepNight) {
+    text = convertNumericQualityToString(item.sleepQuality, context.resources)
+}
+
+@BindingAdapter("sleepImage")
+fun ImageView.setSleepImage(item: SleepNight) {
+    setImageResource(when (item.sleepQuality) {
+        0 -> ic_sleep_0
+        1 -> ic_sleep_1
+        2 -> ic_sleep_2
+        3 -> ic_sleep_3
+        4 -> ic_sleep_4
+        5 -> ic_sleep_5
+        else -> ic_sleep_active
+    })
+}
